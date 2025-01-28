e0f3d27fa5b3d89c1b0650351c1037a5ee6ec290
ulvi05 <aghazadeulvi@gmail.com>
2025-01-28 21:31:27 +0400

    feat(main page): service section is completed

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,13 +1,24 @@
 const menuBtn = document.getElementById("menu-btn");
+const languageBtn = document.getElementById("language-dropdown-btn");
+const languageMenu = document.getElementById("language-dropdown-menu");
 const mobileMenu = document.getElementById("navbar-language");
 import { setupDarkModeToggle } from "../../components/darkModeToggle.js";
 
 setupDarkModeToggle();
 
-menuBtn?.addEventListener("click", function () {
+menuBtn?.addEventListener("click", () => {
   mobileMenu?.classList.toggle("hidden");
 });
 
-menuBtn.addEventListener("click", function () {
-  mobileMenu.classList.toggle("hidden");
+languageBtn?.addEventListener("click", () => {
+  languageMenu?.classList.toggle("hidden");
+});
+
+document.addEventListener("click", (event) => {
+  if (
+    !languageBtn.contains(event.target) &&
+    !languageMenu.contains(event.target)
+  ) {
+    languageMenu?.classList.add("hidden");
+  }
 });
