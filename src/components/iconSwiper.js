52e7f364b61b306db89af29335e1453eb7c9d8c4
ulvi05 <aghazadeulvi@gmail.com>
2025-02-03 02:40:44 +0400

    feat(main page): technology section added

---
+++ src/components/iconSwiper.js
@@ -0,0 +1,10 @@
+const iconSwiper = new Swiper(".icon-slider-swiper", {
+  slidesPerView: 6,
+  spaceBetween: 3,
+  loop: true,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+  speed: 3000,
+});
