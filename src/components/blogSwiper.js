0c73f3ed352ba2dd22d60a44562cf7c49af1f865
ulvi05 <aghazadeulvi@gmail.com>
2025-03-08 02:23:15 +0400

    feat(home page): 2 section added

--- src/components/blogSwiper.js
+++ src/components/blogSwiper.js
@@ -1,7 +1,11 @@
-const swiper = new Swiper(".swiper", {
-  slidesPerView: 1,
-  spaceBetween: 10,
+const swiper = new Swiper(".blog-swiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
   loop: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
@@ -11,19 +15,15 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
   breakpoints: {
-    640: {
+    480: {
       slidesPerView: 1,
-      spaceBetween: 15,
+      spaceBetween: 10,
     },
     768: {
       slidesPerView: 2,
       spaceBetween: 20,
     },
     1024: {
-      slidesPerView: 2,
-      spaceBetween: 25,
-    },
-    1280: {
       slidesPerView: 3,
     },
   },
